ef2c2e1e7613a8778e5a66a0f53169ab30022881
Jung ui heon <ky7662@naver.com>
2023-11-18 22:09:47 +0900

    feat : first commit

---
+++ RollyTest1/src/main/java/com/example/RollyTest1/Dto/AuthorityDto.java
@@ -0,0 +1,13 @@
+package com.example.RollyTest1.Dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
---
+++ RollyTest1/src/test/java/com/example/RollyTest1/RollyTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RollyTest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollyTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
